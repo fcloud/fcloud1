36bcbbc222ec0277ca36595c0a3212316bed7e9c
fyxruben <fyxruben@gmail.com>
2014-02-24 23:13:28 -0800

    怎讲model排序

--- src/main/java/com/fcloud/core/repository/support/TableCreator.java
+++ src/main/java/com/fcloud/core/repository/support/TableCreator.java
@@ -1,5 +1,6 @@
 package com.fcloud.core.repository.support;
 
+import com.fcloud.core.repository.TableOrder;
 import com.fcloud.util.scanning.ClassPathScanningClassesExecutor;
 import com.j256.ormlite.support.ConnectionSource;
 import com.j256.ormlite.table.DatabaseTable;
@@ -8,13 +9,13 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import java.sql.SQLException;
-import java.util.Collections;
-import java.util.Set;
+import java.util.*;
 
 /**
  * @author Ruben Fu
@@ -39,13 +40,18 @@ public class TableCreator {
     public void maybeCreateTable() throws SQLException {
         createTables = new ClassPathScanningClassesExecutor().addAnnotationIncludeTypeFilter(
                 DatabaseTable.class).findCandidateClasses(basePackage);
+
+        List<Class<Object>> list = corverToList(createTables);
+
+        System.out.println(list);
+
         logger.info("createType:" + createType);
         logger.info("basePackage:" + basePackage);
         if ("create".equalsIgnoreCase(createType) || "create-drop".equalsIgnoreCase(createType)) {
             doDrop();
         }
         if ("create".equalsIgnoreCase(createType) || "create-drop".equalsIgnoreCase(createType)) {
-            for (Class<?> clz : createTables) {
+            for (Class<?> clz : list) {
                 logger.info("create class table :" + clz);
                 try {
                     TableUtils.createTable(connectionSource, clz);
@@ -55,7 +61,7 @@ public class TableCreator {
             }
         }
         if ("update".equalsIgnoreCase(createType)) {
-            for (Class<?> clz : createTables) {
+            for (Class<?> clz : list) {
                 logger.info("create class table if not exists :" + clz + " table!");
                 try {
                     TableUtils.createTableIfNotExists(connectionSource, clz);
@@ -66,8 +72,23 @@ public class TableCreator {
         }
     }
 
+    private List<Class<Object>> corverToList(Set<Class<Object>> createTables) {
+        List<Class<Object>> list = new ArrayList<Class<Object>>(createTables);
+        Collections.sort(list, new Comparator<Class<Object>>() {
+            @Override
+            public int compare(Class<Object> c1, Class<Object> c2) {
+                TableOrder o1 = AnnotationUtils.findAnnotation(c1, TableOrder.class);
+                TableOrder o2 = AnnotationUtils.findAnnotation(c2, TableOrder.class);
+                int i1 = o1 == null ? Integer.MAX_VALUE : o1.value();
+                int i2 = o2 == null ? Integer.MAX_VALUE : o2.value();
+                return i1 - i2;
+            }
+        });
+        return list;
+    }
+
     private void doDrop() throws SQLException {
-        for (Class<?> clz : createTables) {
+        for (Class<?> clz : corverToList(createTables)) {
             logger.info("drop class table :" + clz);
             try {
                 TableUtils.dropTable(connectionSource, clz, true);
